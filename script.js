d34e2964042d0fdbd7adc9ddea6e1c55b9cf2279
PikriaP <102671865+PikriaP@users.noreply.github.com>
2022-12-18 22:52:34 +0400

    12

--- script.js
+++ script.js
@@ -342,3 +342,176 @@ const navbar = document.querySelector("#nav-flex");
 window.onscroll = ()=> {
   this.scrollY > 700 ? navbar.classList.add("scroll") : navbar.classList.remove("scroll");
 }
+
+// slider
+let data = [
+  {
+    id: 1,
+    imageUrl: 'https://images.techopedia.com/images/uploads/women-in-tech2.jpg?w=800&h=0&mode=max&quality=70&scale=both',
+    
+  },
+  {
+    id: 2,
+    imageUrl: 'https://elu.nl/wp-content/uploads/2021/03/c1920_womenintech-299653.jpg',
+   
+  },
+  {
+    id: 3,
+    imageUrl: 'https://www.here.com/sites/g/files/odxslz256/files/styles/blog_post_large/public/2022-06/london-office-women-blog.webp?itok=OIcqUPC9',
+    
+  },
+];
+
+
+let leftArrow = document.getElementById('LeftArrow');
+let righttArrow = document.getElementById('RightArrow');
+let SliderBox = document.getElementById('imageContainer');
+let sliderIndex = 0;
+let activedot = document.getElementsByClassName('dots');
+
+function DivTag () {
+let Divi = document.createElement('div');
+Divi.classList.add('ImageBox');
+
+return Divi;
+}
+
+function BGimage (item) {
+let BgImage = document.createElement('div');
+BgImage.style.backgroundImage = `url(${item.imageUrl})`;
+BgImage.classList.add('BgImage');
+
+return BgImage;
+}
+
+function Title (item) {
+let SlideTitle = document.createElement('h3');
+SlideTitle.innerText = item.title;
+SlideTitle.classList.add('Title');
+
+return SlideTitle;
+}
+function CreateDots () {
+let DotBox = document.createElement('div');
+DotBox.classList.add('DotMain');
+
+data.forEach((element) => {
+  let dot = document.createElement("div");    
+  dot.classList.add("dots");
+  dot.setAttribute('data-id', element.id-1);
+  DotBox.appendChild(dot);
+  dot.addEventListener('click', function(event) {
+    let number = event.target.getAttribute("data-id");
+    sliderIndex = number;
+    Slider();
+  });
+});
+
+return DotBox;
+}
+
+function Slider () {
+SliderBox.innerHTML = ' ';
+let MainDiv = DivTag(data[sliderIndex]);
+let Image = BGimage(data[sliderIndex]);
+let imgTitle = Title(data[sliderIndex]);
+let DotParent = CreateDots();
+
+MainDiv.appendChild(Image);
+MainDiv.appendChild(imgTitle);
+SliderBox.appendChild(MainDiv);
+MainDiv.appendChild(DotParent);
+
+activedot[sliderIndex].classList.add('active');
+}
+function ClickLeftArrow () {
+if (sliderIndex == 0) {
+  sliderIndex = data.length-1;
+  Slider();
+  return;
+}
+sliderIndex--;
+Slider();
+}
+leftArrow.addEventListener('click', ClickLeftArrow);
+
+function ClickRighttArrow () {
+if (sliderIndex == data.length-1) {
+  sliderIndex = 0;
+  Slider();
+  return;
+}
+sliderIndex++;
+Slider();
+}
+righttArrow.addEventListener('click', ClickRighttArrow);
+
+setInterval(() => {
+ClickRighttArrow();
+}, 4000);
+
+Slider();
+
+
+// Second homework - form validation
+
+let registrationForm = document.getElementById('RegForm');
+
+registrationForm.addEventListener('submit', function(registration) {
+registration.preventDefault();
+
+let errors = {};
+
+let username = document.getElementById('username').value;
+if (username == '') {
+  errors.UserName = 'Please, enter username';
+}
+
+let password = document.getElementById('password').value;
+let repeatpassword = document.getElementById('Repeatpassword').value;
+
+if (password =='') {
+  errors.Password = 'please, enter password';
+}
+
+if (password != repeatpassword) {
+  errors.RepeatPassword = 'Passwords do not match';
+}
+
+let CheckBox = document.getElementById('agreement').checked;
+if (!CheckBox) {
+  errors.agreement = "Please, agree terms and contiions";
+}
+
+document.querySelectorAll('.ErrorText').forEach((item) => {
+  item.innerText = " ";
+});
+
+for (let key in errors) {
+  let Errortext = document.getElementById("errors_" + key);
+
+  if (Errortext) {
+    Errortext.innerText = errors[key];
+  }
+}
+
+if (Object.keys(errors).length==0) {
+  registrationForm.submit();
+} 
+
+});
+
+let passwordArea = document.getElementById('password');
+let icon = document.getElementById('Icon');
+
+icon.addEventListener("click", function () {
+if (password.type == "password") {
+  passwordArea.setAttribute("type", "text");
+  icon.classList.remove("fa-eye");
+  icon.classList.add("fa-eye-slash");
+} else {
+  passwordArea.setAttribute("type", "password");
+  icon.classList.remove("fa-eye-slash");
+  icon.classList.add("fa-eye");    
+}
+});
